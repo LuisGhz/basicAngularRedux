fc3ca8d2ea96dc85ede05dd4ac6d5347f64df506
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-03-24 16:16:00 -0600

    Change the string by constants

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,11 +1,12 @@
 import { Action } from '@ngrx/store';
+import { INCREMENTAR, DECREMENTAR } from './contador.actions';
 
 export function contadorReducer(state: number = 10, action: Action) {
 
     switch (action.type) {
-        case 'INCREMENTAR':
+        case INCREMENTAR:
             return ++state;
-        case 'DECREMENTAR':
+        case DECREMENTAR:
             return --state;
 
 
