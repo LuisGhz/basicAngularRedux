ccda331b2cae1b4b19ac1cb15a969e6e3bc44338
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-03-30 13:28:05 -0600

    Change the previous method to Redux

--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppState } from '../../app.reducers';
+import { Store } from '@ngrx/store';
+import { ResetCounter } from '../counter.actions';
 
 @Component({
   selector: 'app-nieto',
   templateUrl: './nieto.component.html',
   styles: []
 })
-export class NietoComponent implements OnInit {
+export class NietoComponent {
 
-  @Input() contador: number;
-  @Output() cambioContador = new EventEmitter<number>();
+  counter: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<AppState>) {
+    this.store.select('counter').subscribe(counter => this.counter = counter);
   }
 
   reset() {
-    this.contador = 0;
-    this.cambioContador.emit(this.contador);
+    const action = new ResetCounter();
+
+    this.store.dispatch(action);
   }
 
 }
