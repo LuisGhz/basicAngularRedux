02b03d330f5c30306a31e8a96dc7ca55eedd0ac9
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-03-30 12:16:46 -0600

    Import new actions

--- src/app/contador/counter.reducer.ts
+++ src/app/contador/counter.reducer.ts
@@ -1,14 +1,16 @@
-import { Action } from '@ngrx/store';
-import { INCREMENT, DECREMENT } from './contador.actions';
+import { Actions, INCREMENT, DECREMENT, MULTIPLY, DIVIDE } from './counter.actions';
 
-export function counterReducer(state: number = 10, action: Action) {
+export function counterReducer(state: number = 10, action: Actions) {
 
     switch (action.type) {
         case INCREMENT:
             return ++state;
         case DECREMENT:
             return --state;
-
+        case MULTIPLY:
+            return state * action.payload;
+        case DIVIDE:
+            return state / action.payload;
 
         default:
             return state;
